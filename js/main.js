31fe0a772388f8c244da59268f2c6412d3d7f01a
maxsiokh <maxsiokh@gmail.com>
2024-05-08 14:27:28 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
-window.addEventListener("scroll", function(){
-    var header = document.querySelector(".header-inner");
-    header.classList.toggle("sticky" ,window.scrollY > 0);
-})
-
-
-const galleryImages = document.querySelectorAll('.gallery-image');
-const modal = document.querySelector('.modal');
-const modalContent = document.querySelector('.modal-content');
-const closeModal = document.querySelector('.close');
-
-galleryImages.forEach(image => {
-    image.addEventListener('click', () => {
-        modal.style.display = 'block';
-        modalContent.src = image.src;
-    });
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".header-inner");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-closeModal.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
+const galleryImages = document.querySelectorAll(".gallery-image");
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const closeModal = document.querySelector(".close");
 
-window.addEventListener('click', event => {
-    if (event.target === modal) {
-        modal.style.display = 'none';
+galleryImages.forEach((image) => {
+  image.addEventListener("click", () => {
+    if (window.innerWidth >= 500) {
+      // Перевірка ширини вікна
+      modal.style.display = "flex";
+      modalContent.src = image.src;
     }
+  });
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
 });
